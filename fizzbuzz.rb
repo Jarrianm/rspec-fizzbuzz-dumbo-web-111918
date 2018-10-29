370b28ea25e9d5b087b57a16d6e539a5d3fb589a
Jarrian McLean <jarrian.mclean@gmail.com>
2018-10-29 01:18:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,18 @@
 def fizzbuzz (integer)
-  if (integer % 3)
-	    "Fizz"
-elsif (integer % 5)
-	    "Buzz"
-elsif  (the integer % 3 && 5)
-  return "FizzBuzz"
-else return nil
-  end
+  if ((integer % 3 == 0) && (integer % 5 == 0))
+    return "FizzBuzz"
 
 end
+
+
+
+
+
+# if (integer % 3)
+#   return "Fizz"
+# elsif (integer % 5)
+#     "Buzz"
+# elsif  (the integer % 3 && 5)
+# return "FizzBuzz"
+# else return nil
+# end

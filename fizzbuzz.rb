d8860e3d1a7872fae9f38dbe42c9a933eed99a3e
Jarrian McLean <jarrian.mclean@gmail.com>
2018-10-29 00:59:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
-def fizzbuzz
+def fizzbuzz (integer)
+  if (the number is divisible by 3) then
+	return "Fizz"
+elsif (the number is divisible by 5) then
+	return "Buzz"
+else /* the number is not divisible by 3 or 5 */
+	print the number
+  end if
 
-end 
+end
